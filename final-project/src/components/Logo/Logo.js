8318d3ddb0d409a1a223e1eb63eb7e2c8c4021c9
Marko Bachvarovski <marko.bachvarovski@mail.utoronto.ca>
2022-01-12 00:32:08 +0100

    Added the Image Form Component

--- final-project/src/components/Logo/Logo.js
+++ final-project/src/components/Logo/Logo.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import Tilt from 'react-tilt'
+import Tilt from 'react-tilt';
+import brain from './brain.png';
+import './Logo.css';
 
 const Logo = () => {
     return(
       <div className='ma4 mt0'>
-          <Tilt className="Tilt br2 shadow-2" options={{ max : 25 }} style={{ height: 250, width: 250 }} >
-              <div className="Tilt-inner"> 👽 </div>
+          <Tilt className="Tilt br2 shadow-2" options={{ max : 60 }} style={{ height: 130, width: 130 }} >
+              <div className="Tilt-inner">
+                  <img src={brain} height='130px' width='130px'/>
+              </div>
           </Tilt>
       </div>
     );
