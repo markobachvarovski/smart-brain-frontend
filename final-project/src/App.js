790d8d0d293c185acabdf725a55672250e655b14
Marko Bachvarovski <marko.bachvarovski@mail.utoronto.ca>
2022-01-12 18:26:59 +0100

    Face recognition API now functioning

--- final-project/src/App.js
+++ final-project/src/App.js
@@ -1,10 +1,67 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Rank from './components/Rank/Rank';
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition"
 import Particles from "react-tsparticles";
+import Clarifai from 'clarifai';
+import {calculateNewValue} from "@testing-library/user-event/dist/utils";
+
+const app = new Clarifai.App({
+    apiKey: 'd4859bbc19794718a2c877aa76ed917e'
+});
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            input: '',
+            imgUrl: '',
+            box: {}
+        }
+    }
+
+    calculateFaceLocation = (data) => {
+        const face = data.outputs[0].data.regions[0].region_info.bounding_box
+    }
+
+    onInputChange = (event) => {
+        this.setState({input: event.target.value})
+    }
+
+    onButtonSubmit = () => {
+        this.setState({imgUrl: this.state.input})
+        app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
+            function(response){
+                this.calculateFaceLocation(response)
+            },
+            function(err){
+                console.log(err)
+            }
+        );
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <Particles
+                    className={'particles'}
+                    id="tsparticles"
+                    init={particlesInit}
+                    loaded={particlesLoaded}
+                    options={particleOptions}/>
+                <Navigation />
+                <Logo />
+                <Rank />
+                <ImageLinkForm onInputChange = {this.onInputChange}
+                               onButtonSubmit = {this.onButtonSubmit}/>
+                <FaceRecognition imgUrl={this.state.imgUrl}/>
+            </div>
+        );
+    }
+}
 
 const particlesInit = (main) => {
     console.log(main);
@@ -17,35 +74,35 @@ const particlesLoaded = (container) => {
 const particleOptions = {
     background: {
         gradient: {
-           value: 'linear-gradient(100deg, #0CE8B3 0%, #02B7F7 100%)',
+            value: 'linear-gradient(100deg, #0CE8B3 0%, #02B7F7 100%)',
         }
     },
     fpsLimit: 60,
-        interactivity: {
+    interactivity: {
         events: {
             onClick: {
                 enable: true,
-                    mode: "push",
+                mode: "push",
             },
             onHover: {
                 enable: true,
-                    mode: "repulse",
+                mode: "repulse",
             },
             resize: true,
         },
         modes: {
             bubble: {
                 distance: 400,
-                    duration: 2,
-                    opacity: 0.8,
-                    size: 40,
+                duration: 2,
+                opacity: 0.8,
+                size: 40,
             },
             push: {
                 quantity: 4,
             },
             repulse: {
                 distance: 200,
-                    duration: 0.4,
+                duration: 0.4,
             },
         },
     },
@@ -55,28 +112,28 @@ const particleOptions = {
         },
         links: {
             color: "#ffffff",
-                distance: 150,
-                enable: true,
-                opacity: 0.9,
-                width: 1,
+            distance: 150,
+            enable: true,
+            opacity: 0.9,
+            width: 1,
         },
         collisions: {
             enable: true,
         },
         move: {
             direction: "none",
-                enable: true,
-                outMode: "bounce",
-                random: false,
-                speed: 3,
-                straight: false,
+            enable: true,
+            outMode: "bounce",
+            random: false,
+            speed: 2,
+            straight: false,
         },
         number: {
             density: {
                 enable: true,
-                    area: 800,
+                area: 800,
             },
-            value: 100,
+            value: 150,
         },
         opacity: {
             value: 0.5,
@@ -86,27 +143,10 @@ const particleOptions = {
         },
         size: {
             random: true,
-                value: 5,
+            value: 5,
         },
     },
     detectRetina: true,
-}
-
-function App() {
-  return (
-    <div className="App">
-      <Particles
-          id="tsparticles"
-          init={particlesInit}
-          loaded={particlesLoaded}
-          options={particleOptions}/>
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-        { /*<FaceRecognition /> */}
-    </div>
-  );
 }
 
 export default App;
