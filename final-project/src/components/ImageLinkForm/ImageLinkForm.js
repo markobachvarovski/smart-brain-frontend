e9c794180b7c36672939a844714eb66a89b1d796
Marko Bachvarovski <marko.bachvarovski@mail.utoronto.ca>
2022-01-13 12:07:56 +0100

    Changed the button color

--- final-project/src/components/ImageLinkForm/ImageLinkForm.js
+++ final-project/src/components/ImageLinkForm/ImageLinkForm.js
@@ -11,7 +11,7 @@ const ImageLinkForm = ({onInputChange, onButtonSubmit}) => {
 
                     <input  className={'f4 pa2 w-70 center'} type={"text"} onChange={onInputChange}/>
 
-                    <button className={'w-30 grow f4 link ph3 pv2 dib white bg-green text-black'}
+                    <button className={'w-30 grow f4 link ph3 pv2 dib white text-black'}
                             onClick={onButtonSubmit}>
                         DETECT
                     </button>
