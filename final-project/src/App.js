06073010317913ba399c831d168188b32c6c5270
Marko Bachvarovski <marko.bachvarovski@mail.utoronto.ca>
2022-01-17 14:28:45 +0100

    Updated path to server and deleted backend files

--- final-project/src/App.js
+++ final-project/src/App.js
@@ -86,7 +86,7 @@ class App extends Component {
         app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
             .then(response => {
                 if (response) {
-                    fetch('http://localhost:3001/image', {
+                    fetch('https://limitless-cove-41492.herokuapp.com/image', {
                         method: 'put',
                         headers: {'Content-Type': 'application/json'},
                         body: JSON.stringify({
--- final-project/src/components/Register/Register.js
+++ final-project/src/components/Register/Register.js
@@ -25,7 +25,7 @@ class Register extends React.Component {
     }
 
     onSubmitRegister = () => {
-        fetch('http://localhost:3001/register',{
+        fetch('https://limitless-cove-41492.herokuapp.com/register',{
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
