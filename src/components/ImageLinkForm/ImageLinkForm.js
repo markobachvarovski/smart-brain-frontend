01446346fbdae6a89e8714373120ab22c3e34a16
Marko Bachvarovski <marko.bachvarovski@mail.utoronto.ca>
2022-01-17 19:00:46 +0100

    Added more instructions to the app

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -5,7 +5,8 @@ import './ImageLinkForm.css';
 const ImageLinkForm = ({onInputChange, onButtonSubmit}) => {
     return(
         <div>
-            <p className={'f3'}>This Magic Brain will detect faces in your pictures. Give it a try!</p>
+            <p className={'f3'}>This Magic Brain will detect faces in your pictures.
+                Give it a try by entering an URL of an image!</p>
             <div className={'center'}>
                 <div className={'center form pa4 br15 shadow-5'}>
                     <input  id={'imageInput'} className={'f4 pa2 br15 w-75 center'} type={"text"} onChange={onInputChange}/>
